8123ce122fb93f5dd1dbf7fcac003e7a404e4140
Mico Piira <mico.piira@gmail.com>
2017-01-25 20:56:40 +0200

    Change all indents to tabs

--- src/client.jsx
+++ src/client.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
+import {render} from 'react-dom'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
 import * as reducers from './reducers'
-import { Router, browserHistory } from 'react-router';
+import {Router, browserHistory} from 'react-router';
 import createLogger from 'redux-logger';
 import Routes from './routes';
 import thunk from 'redux-thunk';
@@ -13,8 +13,8 @@ const preloadedState = window.__PRELOADED_STATE__;
 const store = createStore(combineReducers(reducers), preloadedState, applyMiddleware(thunk, createLogger()));
 
 render(
-    <Provider store={store}>
-        <Router history={browserHistory} routes={Routes}/>
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<Router history={browserHistory} routes={Routes}/>
+	</Provider>,
+	document.getElementById('root')
 );
