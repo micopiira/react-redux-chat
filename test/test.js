c591df390be21c4cf7dd792688fef47adf913d43
Mico Piira <mico.piira@gmail.com>
2017-01-28 21:49:54 +0200

    Remove console.log from unit test

--- test/test.js
+++ test/test.js
@@ -8,12 +8,11 @@ chai.use(chaiHttp);
 
 describe('Test', () => {
 	it('should return 200', done => {
-		chai.request(server).get('/').then(function (res) {
-			console.log(res);
+		chai.request(server).get('/').then(res => {
 			expect(res).to.have.status(200);
 			done();
 		})
-		.catch(function (err) {
+		.catch(err => {
 			throw err;
 		});
 	}).timeout(15000);
