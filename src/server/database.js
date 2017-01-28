f2f8ed5828c6a2207c34ec500674a74a4abf044c
Mico Piira <mico.piira@gmail.com>
2017-01-28 21:38:25 +0200

    modified:   src/server/database.js

--- src/server/database.js
+++ src/server/database.js
@@ -5,38 +5,22 @@ import path from 'path';
 
 let db = {...data};
 
-/**
- * @typedef {Object} Message
- * @property {String} text
- */
-
-/**
- * @typedef {Object} Page.<T>
- * @property {Array.<T>} content
- * @property {Number} totalElements
- */
-
-/**
- * @typedef {Object} PageRequest
- * @property {Number} page - zero-based page index.
- * @property {Number} size - the size of the page to be returned.
- */
+const save = cb => {
+	fs.writeFile(path.join(__dirname, '../data.json'), JSON.stringify(db), 'utf8', cb);
+};
 
 const api = {
-	/**
-	 *
-	 * @param {PageRequest} pageRequest
-	 * @return {Promise.<Page.<Message>>}
-	 */
 	getMessages: pageRequest => Promise.resolve({
 		totalElements: db.messages.length,
-		content: db.messages.slice().reverse().slice(pageRequest.page * pageRequest.size, pageRequest.page * pageRequest.size + pageRequest.size).reverse()
+		content: db.messages.slice()
+			.reverse()
+			.slice(pageRequest.page * pageRequest.size, pageRequest.page * pageRequest.size + pageRequest.size)
+			.reverse()
 	}),
 	addMessage: message => {
 		const msg = {...message, id: shortid.generate(), timestamp: new Date()};
-		db = {...db, messages: db.messages.concat(msg)};
-		fs.writeFile(path.join(__dirname, '../data.json'), JSON.stringify(db), 'utf8', () => {
-		});
+		db = {...db, messages: (db.messages || []).concat(msg)};
+		save(() => console.log('saved to file'));
 		return msg;
 	}
 };
