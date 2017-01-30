6fd144c25b4dc43a42e50da7e5e7129fc15cee2a
Mico Piira <mico.piira@gmail.com>
2017-01-30 15:14:59 +0200

    Add test case for 404 routes

--- test/test.js
+++ test/test.js
@@ -7,16 +7,23 @@ const http = require('http');
 
 chai.use(chaiHttp);
 
-describe('Test', () => {
-	it('should return 200', done => {
-		chai.request(server).get('/').then(res => {
-			expect(res).to.have.status(200);
-			done();
-		})
-		.catch(err => {
-			throw err;
+describe('Routes', () => {
+	describe('GET /', () => {
+		it('should return 200', done => {
+			chai.request(server).get('/').end((err, res) => {
+				expect(res).to.have.status(200);
+				done(err);
+			});
+		}).timeout(15000);
+	});
+	describe('GET /404', () => {
+		it('should return 404', done => {
+			chai.request(server).get('/404').end((err, res) => {
+				expect(res).to.have.status(404);
+				done();
+			});
 		});
-	}).timeout(15000);
+	});
 	after(() => {
 		server.close();
 	});
