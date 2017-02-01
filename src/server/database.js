5b6c0fe168b6bc18bbcbc9a71c1b98f3b930fcbe
Mico Piira <mico.piira@gmail.com>
2017-02-01 18:56:38 +0200

    clean database.js

--- src/server/database.js
+++ src/server/database.js
@@ -1,27 +1,29 @@
 import shortid from 'shortid';
 import storage from 'node-persist';
 
-storage.initSync();
-
 const MESSAGES_KEY = 'messages';
+
+storage.initSync();
 
 if (!storage.getItemSync(MESSAGES_KEY)) {
 	storage.setItemSync(MESSAGES_KEY, []);
 }
 
+const arrayToPage = (array, pageRequest) => ({
+	totalElements: array.length,
+	content: array.slice()
+		.reverse()
+		.slice(pageRequest.page * pageRequest.size, pageRequest.page * pageRequest.size + pageRequest.size)
+		.reverse()
+});
+
 const api = {
-	getMessages: pageRequest => storage.getItem(MESSAGES_KEY).then(messages => ({
-		totalElements: messages.length,
-		content: messages.slice()
-			.reverse()
-			.slice(pageRequest.page * pageRequest.size, pageRequest.page * pageRequest.size + pageRequest.size)
-			.reverse()
-	})),
+	getMessages: pageRequest => storage.get(MESSAGES_KEY).then(messages => arrayToPage(messages, pageRequest)),
 	addMessage: message => {
 		const msg = {...message, id: shortid.generate(), timestamp: new Date()};
-		return storage.getItem('messages')
+		return storage.getItem(MESSAGES_KEY)
 			.then(messages => messages.concat(msg))
-			.then(messages => storage.setItem('messages', messages))
+			.then(messages => storage.set(MESSAGES_KEY, messages))
 			.then(() => msg);
 	}
 };
