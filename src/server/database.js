4cfc86d8e0632153f63175dd082e8889ff1b6976
Mico Piira <mico.piira@gmail.com>
2017-02-06 17:09:00 +0200

    fix invalid prop type

--- src/server/database.js
+++ src/server/database.js
@@ -20,7 +20,7 @@ const arrayToPage = (array, pageRequest) => ({
 const api = {
 	getMessages: pageRequest => storage.get(MESSAGES_KEY).then(messages => arrayToPage(messages, pageRequest)),
 	addMessage: message => {
-		const msg = {...message, id: shortid.generate(), timestamp: new Date()};
+		const msg = {...message, id: shortid.generate(), timestamp: new Date().toISOString()};
 		return storage.getItem(MESSAGES_KEY)
 			.then(messages => messages.concat(msg))
 			.then(messages => storage.set(MESSAGES_KEY, messages))
