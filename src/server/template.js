758752a45030cdb7f858b0e18de8b51138072599
Mico Piira <mico.piira@gmail.com>
2017-03-29 20:01:46 +0300

    Add some meta tags and package.json fields

--- src/server/template.js
+++ src/server/template.js
@@ -1,15 +1,24 @@
 import config from '../../config.json';
+import packageJson from '../../package.json';
+
+const getAssetsByExtension = (assets, extension, mapFn) =>
+	assets.app
+		.filter(path => path.endsWith(extension))
+		.map(mapFn)
+		.join('');
 
 export default (html, preloadedState, assetsByChunkName) => {
-	const styles = assetsByChunkName.app.filter(path => path.endsWith('.css')).map(path => `<link rel="stylesheet" href="${path}">`).join('');
-	const scripts = assetsByChunkName.app.filter(path => path.endsWith('.js')).map(path => `<script src="${path}"></script>`).join('');
-	return `
-		<!doctype html>
+	const styles = getAssetsByExtension(assetsByChunkName, '.css', path => `<link rel="stylesheet" href="${path}">`);
+	const scripts = getAssetsByExtension(assetsByChunkName, '.js', path => `<script src="${path}"></script>`);
+	return (
+	`<!doctype html>
 		<html lang="en">
 			<head>
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<meta name="description" content="${packageJson.description}">
+				<meta name="author" content="${packageJson.author.name}">
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 				<title>${config.title}</title>
 				${styles}
@@ -19,5 +28,6 @@ export default (html, preloadedState, assetsByChunkName) => {
 				<script>window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')};</script>
 				${scripts}
 			</body>
-		</html>`.replace(/\n\s+/g, '');
+		</html>`
+	);
 };
